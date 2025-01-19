09cca4075a579f0f1249737396590546bfb38d51
Jatish Khanna <jatish.khanna@gmail.com>
2025-01-19 00:23:20 -0700

    Grading System with TreeMap implemented

--- src/StudentGradingSystem.java
+++ src/StudentGradingSystem.java
@@ -1,7 +1,75 @@
+import java.util.*;
+
 public class StudentGradingSystem {
 
+  static class Student {
+    String name;
+    int grade;
+
+    Student(String name, int grade) {
+      this.name = name;
+      this.grade = grade;
+    }
+
+    public String toString() {
+      return name + " " + grade;
+    }
+  }
+
+  TreeMap<Integer, Set<Student>> studentsByGrade = new TreeMap<>();
+  Map<String, Student> studentsByName = new HashMap<>();
+
+  private Collection<Set<Student>> getStudentsInGradeRange(int g1, int g2) {
+
+    var studentsInRange = studentsByGrade.subMap(g1, true, g2, true);
+    return studentsInRange.values();
+
+  }
+
+  private Set<Student> getLowestStudent() {
+    var students = studentsByGrade.firstEntry();
+    return students == null ? Set.of() : students.getValue();
+  }
+
+  private Set<Student> getTopStudent() {
+    var students = studentsByGrade.lastEntry();
+    return students == null ? Set.of() : students.getValue();
+  }
+
+  private int getGrade(String name) {
+    Student student = studentsByName.get(name);
+    return student == null ? -1 : student.grade;
+  }
+
+  private void addStudent(String name, int grade) {
+    Student student = studentsByName.get(name);
+    if (student == null) {
+      student = new Student(name, grade);
+    } else if (student.grade == grade) {
+      return;
+    } else {
+      student = removeStudent(name); // we have checked student will always exists
+      student.grade = grade;
+    }
+    studentsByName.put(name, new Student(name, grade));
+    studentsByGrade.computeIfAbsent(grade, (g) -> new HashSet<>()).add(student);
+
+  }
+
+  private Student removeStudent(String name) {
+    if (!studentsByName.containsKey(name)) {
+      return null;
+    }
+    Student student = studentsByName.remove(name);
+    studentsByGrade.get(student.grade).remove(student);
+    if (studentsByGrade.get(student.grade).isEmpty()) {
+      studentsByGrade.remove(student.grade);
+    }
+    return student;
+  }
+
   public static void main(String[] args) {
-    StudentGrades studentGrades = new StudentGrades();
+    StudentGradingSystem studentGrades = new StudentGradingSystem();
     studentGrades.addStudent("Alice", 95);
     studentGrades.addStudent("Bob", 89);
     studentGrades.addStudent("Charlie", 72);
@@ -13,4 +81,5 @@ public class StudentGradingSystem {
 
     System.out.println("Students with grades between 80 and 95: " + studentGrades.getStudentsInGradeRange(80, 95));
   }
+
 }
